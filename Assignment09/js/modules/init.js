fc19bb19290c21ed2a90c16169e988affe2d41c2
priyarakkan <124619499+priyarakkan@users.noreply.github.com>
2023-04-19 16:57:02 -0700

    Add files via upload

---
+++ Assignment09/js/modules/init.js
@@ -0,0 +1,24 @@
+const xhr = new XMLHttpRequest()
+xhr.responseType = 'json'
+
+xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        let employees = xhr.response
+        for (let employee of employees) {
+            tbody.innerHTML += 
+            `
+            <tr>
+                <td>${employee[0]}</td>
+                <td>${employee[1]}</td>
+                <td>${employee[2]}</td>
+                <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+                <td>${employee[4]}</td>
+                <td><button class="btn btn-sm btn-danger delete">X</button></td>
+            </tr>
+            `
+        }
+    }
+}
+xhr.open('GET', 'data/employees.json')      //open the connection
+xhr.send()                                   //initate 
+xhr.onerror = (e) => {console.log(e.message)}
