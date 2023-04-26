b2bd7b51eba654442f8727cbc09a26c14041806c
priyarakkan <priyadharsinikdp@gmail.com>
2023-04-25 19:44:16 -0700

    Update init.js

--- Assignment09/js/modules/init.js
+++ Assignment09/js/modules/init.js
@@ -1,24 +1,65 @@
-const xhr = new XMLHttpRequest()
-xhr.responseType = 'json'
+// const xhr = new XMLHttpRequest()
+// xhr.responseType = 'json'
 
-xhr.onreadystatechange = () => {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        let employees = xhr.response
-        for (let employee of employees) {
-            tbody.innerHTML += 
-            `
-            <tr>
-                <td>${employee[0]}</td>
-                <td>${employee[1]}</td>
-                <td>${employee[2]}</td>
-                <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
-                <td>${employee[4]}</td>
-                <td><button class="btn btn-sm btn-danger delete">X</button></td>
-            </tr>
-            `
+// xhr.onreadystatechange = () => {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         let employees = xhr.response
+//         for (let employee of employees) {
+//             tbody.innerHTML += 
+//             `
+//             <tr>
+//                 <td>${employee[0]}</td>
+//                 <td>${employee[1]}</td>
+//                 <td>${employee[2]}</td>
+//                 <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+//                 <td>${employee[4]}</td>
+//                 <td><button class="btn btn-sm btn-danger delete">X</button></td>
+//             </tr>
+//             `
+//         }
+//     }
+// }
+// xhr.open('GET', '/data/employees.json')      //open the connection
+// xhr.send()                                   //initate 
+// xhr.onerror = (e) => {console.log(e.message)}
+
+fetch('/data/employees.json')
+.then(response => response.json())
+.then(data => {
+    for (let employee of employees) {
+                    tbody.innerHTML += 
+                    `
+                    <tr>
+                        <td>${employee[0]}</td>
+                        <td>${employee[1]}</td>
+                        <td>${employee[2]}</td>
+                        <td><a href="mailto:${employee[3]}">${employee[3]}</a></td>
+                        <td>${employee[4]}</td>
+                        <td><button class="btn btn-sm btn-danger delete">X</button></td>
+                    </tr>
+                    `
+                }
+    // KEEP THE USER ON THE COMMAND MENU
+    do {
+        //ALLOW THE USER TO ENTER A COMMAND
+        let command = prompt('Enter a command')
+        if (command !== null) {
+            command = command.toLowerCase()
+            if (command === 'add') {
+
+            } else if (command === 'del') {
+
+            } else if (command === 'exit') {
+
+            } else {
+                alert('Please enter a valid command')
+            }
+        } else {
+            alert('Please enter a command')
         }
-    }
-}
-xhr.open('GET', '/data/employees.json')      //open the connection
-xhr.send()                                   //initate 
-xhr.onerror = (e) => {console.log(e.message)}
+    } while(true)
+    console.log('The application has terminated')
+})
+.catch(e => console.log(e.message))
+
+init()
